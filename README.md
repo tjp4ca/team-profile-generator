d0f05cf4b4ddea10e8fcfb271556eabb7b874720
Michael Park <tjp4ca@gmail.com>
2022-07-27 23:48:59 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # team-profile-generator
+
+screencastify: https://drive.google.com/file/d/1oqW2LGLOBje0jK2eQ9thn4hkScMwrGr0/view
+github link: https://github.com/tjp4ca/team-profile-generator
+html generated in dist folder: https://github.com/tjp4ca/team-profile-generator/blob/main/dist/index.html
--- index.js
+++ index.js
@@ -3,70 +3,12 @@ const inquirer = require('inquirer');
     // console.log('inquirer working');
 
 const fs = require("fs");
-
-// template to generate index.html
-// const generatePage = require('./src/page-template');
-// const { writeFile, copyFile } = require('./utils/generate-site');
-
 const {pageTemplate} = require('./src/page-template');
 
-// const Employee = ('./lib/Employee')
-// const Engineer = ('./lib/Engineer');
-// const Inter = ('./lib/Intern');
-// const Manager = ('./lib/Manager');
-
-
-// const promptQuestions = () => {
-//     return inquirer
-//         .prompt([
-//             // {
-//             // type: 'input',
-//             // name: 'name',
-//             // message: 'What is your name? (Required)',
-//             // validate: nameInput => {
-//             //     if (nameInput) {
-//             //     return true;
-//             //     } else {
-//             //     console.log('Please enter your name!');
-//             //     return false;
-//             //     }
-//             // }
-//             // },
-//             {
-//                 type: 'list',
-//                 name: 'member',
-//                 message: 'Add an engineer or an intern?',
-//                 choices: ['Engineer', 'Intern']
-//             },
-//         ]);
-// };
-
-
-// function promptQuestions() {
-    
-// }
-
-
-// promptQuestions()
-// .then(promptProject)
-// .then(portfolioData => {
-//   return generatePage(portfolioData);
-// })
-// .then(pageHTML => {
-//   return writeFile(pageHTML);
-// })
-// .then(writeFileResponse => {
-//   console.log(writeFileResponse);
-//   return copyFile();
-// })
-// .then(copyFileResponse => {
-//   console.log(copyFileResponse);
-// })
-// .catch(err => {
-//   console.log(err);
-// });
-
-
+const Employee = ('./lib/Employee')
+const Engineer = ('./lib/Engineer');
+const Inter = ('./lib/Intern');
+const Manager = ('./lib/Manager');
 
 const questions = [
     {
@@ -146,20 +88,3 @@ function init() {
 
 // Initialize app
 init();
-
-
-
-
-
-// fs.writeFileSync("index.html", HTML, (err) => {
-//     if (err)
-// })
-
-
-// // Create a function to write README file
-// function writeToFile(fileName, data) {
-//     console.log(data)
-//     fs.writeFile('index.html', data, err =>
-//     err ? console.log(err) : console.log('Success!'));
-// }
-
